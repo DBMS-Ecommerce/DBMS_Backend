ecd2713bd1f1afc969585599749c0284fcfc718f
Dinuka-Thathsara <dinukathathsara@gmail.com>
2022-06-29 07:59:03 +0530

    Initial Changes

--- index.js
+++ index.js
@@ -1,29 +1,68 @@
-const express = require('express'),
-app = express(), 
-cors = require('cors'),
-bodyParser = require('body-parser');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const path = require("path");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+app.use(cors());
+app.use(bodyParser.json());
+// starting the server
+// require("dotenv").config();
+var db;
 
+function handleDisconnect() {
+    db = mysql.createConnection({
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASS,
+        database: process.env.DATABASE_NAME,
+        port: process.env.DATABASE_PORT
+    });
 
-app.get('/',(req,res) =>{
+    db.connect((err) => {
+        if (err) {
+            console.log('error when connecting to db: ', err);
+            setTimeout(handleDisconnect, 2000);
+        }
+    });
+
+    db.on('error', (err) => {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect();
+        } else {
+            throw err;
+        }
+    });
+}
+
+handleDisconnect();
+app.get("/test", (req, res) => {
     res.send("Hello World");
-  })
+});
+app.post("/add_category", (req, res) => {
+    const { title } = req.body;
+    const sql = "INSERT INTO category (title) VALUES (?)";
+    db.query(sql, [title], (err, res) => {
+
+        if (err) {
+            console.log("Error Category Adding" + err);
+            res.json({
+                success: false,
+                err
+            })
+        } else {
+
+            res.send("Category Added");
+            res.json({
+                success: true,
+                result
+            })
+        }
+    })
+
+});
 
 // use the modules
-app.use(cors())
-app.use(bodyParser.json());
 
-
-
-
-
-// starting the server
-const port =  process.env.PORT || 5000;
-app.listen( port , () => console.log(`Server started, listening port: ${port}`));
-
-
-
-
-
-
-
-
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started, listening port: ${port}`));
