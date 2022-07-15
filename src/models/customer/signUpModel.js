f29e223ecaa46d3a084140ec39f3c75d61e5e972
Nuwan-alt <jayasankanuwan1998@gmail.com>
2022-07-16 02:46:49 +0530

    merge

--- src/models/customer/signUpModel.js
+++ src/models/customer/signUpModel.js
@@ -10,13 +10,7 @@ async function signUpUser (req,res){
 
     const user = req.user;
     console.log(user)
-    // const   userName = req.body.userName
-    // const   password = req.body.password
-    // const   userType = req.body.userType
-    // const   phone_number = req.body.phone_num
-    // const   address = req.body.address
-    // const   user_id = uuidv4()
-    
+
     
     const signUpQuery1 = "INSERT  INTO user (userName,password,user_type,user_id,name) VALUES (?,?,?,?,?)"
     const signUpQuery2 = "INSERT  INTO customer (phone_number,address,user_id) VALUES (?,?,?)"
