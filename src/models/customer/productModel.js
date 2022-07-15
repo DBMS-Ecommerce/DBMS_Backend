aab174dab2750d4e3a625f0363a90c713dd366a2
Nuwan-alt <jayasankanuwan1998@gmail.com>
2022-07-15 23:00:20 +0530

    login and signup functions updated

--- src/models/customer/productModel.js
+++ src/models/customer/productModel.js
@@ -24,7 +24,23 @@ async function getAllProducts(req,res){
 
 async function getProductsVar(req,res){
     const pro_id = req.params.id;
-    const stmt = "SELECT "
+    const stmt = "SELECT * FROM `selected_var` WHERE `product_id` = ? "
+
+    return new Promise((resolve,reject)=>{
+        db.query(stmt,[pro_id],function (err, result, fields) {
+                if (err)
+                    throw err;
+                if (result.length == 0) {
+                    console.log("No Item Variant");
+                    reject(new Error("No var"));
+                } else {
+                    console.log(result)
+                    resolve(result);
+
+                }
+            })
+
+    })
 }
 
-module.exports = {getAllProducts}
+module.exports = {getAllProducts,getProductsVar}
--- src/models/customer/signUpModel.js
+++ src/models/customer/signUpModel.js
@@ -20,38 +20,59 @@ async function signUpUser (req,res){
     
     const signUpQuery1 = "INSERT  INTO user (userName,password,user_type,user_id) VALUES (?,?,?,?)"
     const signUpQuery2 = "INSERT  INTO customer (phone_number,address,user_id) VALUES (?,?,?)"
+    const checkUser = "select username from user where username = ?";
 
-    
+    return new Promise((resolve,reject)=>{
+        db.query(checkUser,[user.userName],function (err, result, fields) {
+                if (err)
+                    throw err;
+                    console.log(result)
+                if (result.length != 0) {
+                    
+                    res.status(400).send("user exists")
+                    
+                } else {
+                    
+                    
+
+                    var trCon = transaction({
+                        connection: [mysql.createConnection,{
+                
+                          user: process.env.DB_USER,
+                          password: process.env.DB_PASSWORD,
+                          database: process.env.DB_DATABASE
+                
+                        }],
+                        dynamicConnection: 32,
+                        idleConnectionCutoffTime: 1000,
+                        timeout:600
+                
+                    });
+                
+                    let chain = trCon.chain();
+                
+                    chain.
+                    on('commit', function(){
+                        
+                        res.status(200).send("Data Inserted")
+                        return "Data Inserted";
+                    }).
+                    on('rollback', function(err){
+                       return "Data Insertion Failed";
+                    });
+                
+                    chain.
+                    query(signUpQuery1,[user.userName,user.password,user.userType,user.user_id]).
+                    query(signUpQuery2,[user.phone_number,user.address,user.user_id]);
+                    resolve(result);
+
+                }
+            })
+
+    })
     
    
-        var trCon = transaction({
-            connection: [mysql.createConnection,{
-    
-              user: process.env.DB_USER,
-              password: process.env.DB_PASSWORD,
-              database: process.env.DB_DATABASE
-    
-            }],
-            dynamicConnection: 32,
-            idleConnectionCutoffTime: 1000,
-            timeout:600
-    
-        });
-    
-        let chain = trCon.chain();
-    
-        chain.
-        on('commit', function(){
-            console.log("Data Inserted")
-            return "Data Inserted";
-        }).
-        on('rollback', function(err){
-           return "Data Insertion Failed";
-        });
-    
-        chain.
-        query(signUpQuery1,[user.userName,user.password,user.userType,user.user_id]).
-        query(signUpQuery2,[user.phone_number,user.address,user.user_id]);
+        
       
 }
 
