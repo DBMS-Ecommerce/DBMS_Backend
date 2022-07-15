0c4c49e2f13ed93549e853a596873540b177e78f
Nuwan-alt <jayasankanuwan1998@gmail.com>
2022-07-15 18:50:50 +0530

    get product by id done

--- src/models/customer/productModel.js
+++ src/models/customer/productModel.js
@@ -3,7 +3,7 @@ const db = require('../../config/database');
 async function getAllProducts(req,res){
     const sub_cat_id = req.params.id;
     console.log(sub_cat_id);
-    const stmt  = "SELECT * FROM item WHERE product_id IN (SELECT `product_id` FROM `product` WHERE `sub_category_id` =? )";
+    const stmt  = "SELECT * FROM item WHERE product_id IN (SELECT `product_id` FROM `product` WHERE `sub_category_id` =? ) AND `is_default` = 1";
     
     return new Promise((resolve,reject)=>{
         db.query(stmt,[sub_cat_id],function (err, result, fields) {
@@ -20,6 +20,11 @@ async function getAllProducts(req,res){
             })
 
     })
+}
+
+async function getProductsVar(req,res){
+    const pro_id = req.params.id;
+    const stmt = "SELECT "
 }
 
 module.exports = {getAllProducts}
